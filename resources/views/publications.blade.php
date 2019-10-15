d7d030aaf55a78bca7976593f97fac53e6ca0735
Enrique Rodriguez <enrique.rv0704@gmail.com>
2019-10-14 21:25:09 -0500

    what is pneumo

--- resources/views/publications.blade.php
+++ resources/views/publications.blade.php
@@ -13,10 +13,16 @@
 
 	<div class="container publish mt-5">
 		<div class="row">
-			<div class="col-md-2 text-center">
+			<div class="col-md-3 text-center">
 				<a href="{{ env('APP_URL') }}/assets/files/diptico.pdf" target="_blank">
-					<p><i class="far fa-file-pdf icon"></i></p>
+					<p class="mb-2"><i class="far fa-file-pdf icon"></i></p>
 					<p>Díptico neumonia</p>	
+				</a>
+			</div>
+			<div class="col-md-3 text-center">
+				<a href="{{ env('APP_URL') }}/assets/files/pneumonia.pdf" target="_blank">
+					<p class="mb-2"><i class="far fa-file-pdf icon"></i></p>
+					<p>What is Pneumonia?</p>	
 				</a>
 			</div>
 		</div>
